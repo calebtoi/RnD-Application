2e921067399e94179984dbafe1d845149a0161b1
Caleb Toi <calebtjtoi@gmail.com>
2018-10-08 17:37:23 +1300

    Add basic route retrieval functionality

---
+++ app/src/main/java/development/calebtoi/test/RouteInfoActivity.java
@@ -0,0 +1,13 @@
+package development.calebtoi.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouteInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_info);
+    }
+}
